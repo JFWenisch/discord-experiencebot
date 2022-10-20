05d751bad0f090386b5c0b4d208f0b8db67884ec
Jean-Fabian Wenisch <2443871+JFWenisch@users.noreply.github.com>
2022-10-20 15:53:36 +0200

    feat: added sentry.io integration (#8)

--- discord-bot/src/main/java/tech/wenisch/discord/experiencebot/UpdateRegularsThread.java
+++ discord-bot/src/main/java/tech/wenisch/discord/experiencebot/UpdateRegularsThread.java
@@ -2,54 +2,61 @@ package tech.wenisch.discord.experiencebot;
 
 import java.util.List;
 
+import io.sentry.ITransaction;
+import io.sentry.Sentry;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceJoinEvent;
 
-public class UpdateRegularsThread extends Thread
-{
+public class UpdateRegularsThread extends Thread {
 	GuildVoiceJoinEvent event;
-	UpdateRegularsThread(GuildVoiceJoinEvent event)
-	{
-		this.event=event;
-	}
-	public void run() 
-	{
-		String roleName = event.getGuild().getName().toUpperCase()+" REGULARS";
-		List<String> assignableMembers= Bot.getDatabaseConnection().getRegulars(event.getGuild().getId());
-		List<Role> roles = event.getGuild().getRolesByName(roleName, true);
-		Role destRole =null;
-		if(roles.size()<1)
-		{
-			destRole = event.getGuild().createRole().setName(roleName).complete();
-			System.out.println("Successfully created role "+destRole);
-		}
-		else
-		{
-			destRole = roles.get(0);
-		}
-		final Role createdRole = destRole;
-		// Add assignable Members
-		for(int i=0; i < assignableMembers.size(); i++)
-		{
-			event.getGuild().addRoleToMember(assignableMembers.get(i), destRole).queue();
-			System.out.println("Added member " +assignableMembers.get(i) +" to regular role");
-		}
 
-		List<Member> members = event.getGuild().findMembers(member -> {
-			if (member.getRoles().contains(createdRole)){
-				return true;
+	UpdateRegularsThread(GuildVoiceJoinEvent event) {
+		this.event = event;
+	}
+
+	public void run() {
+		ITransaction transaction = null;
+		if (SentryManager.getInstance().isActivated()) {
+			transaction = Sentry.startTransaction("UpdateRegulars", event.getGuild().getName().toUpperCase());
+		}
+		try {
+			String roleName = event.getGuild().getName().toUpperCase() + " REGULARS";
+			List<String> assignableMembers = Bot.getDatabaseConnection().getRegulars(event.getGuild().getId());
+			List<Role> roles = event.getGuild().getRolesByName(roleName, true);
+			Role destRole = null;
+			if (roles.size() < 1) {
+				destRole = event.getGuild().createRole().setName(roleName).complete();
+				System.out.println("Successfully created role " + destRole);
+			} else {
+				destRole = roles.get(0);
 			}
-			return false;
-		}).get();
-		// Remove not qualified members
-		for(int i=0; i < members.size(); i++)
-		{
-			if(!assignableMembers.contains(members.get(i).getId()))
-			{
-				event.getGuild().removeRoleFromMember(members.get(i), createdRole).queue();
-				System.out.println("Removed member " +members.get(i) +" from regular role");
+			final Role createdRole = destRole;
+			// Add assignable Members
+			for (int i = 0; i < assignableMembers.size(); i++) {
+				event.getGuild().addRoleToMember(assignableMembers.get(i), destRole).queue();
+				System.out.println("Added member " + assignableMembers.get(i) + " to regular role");
 			}
-		}    
+
+			List<Member> members = event.getGuild().findMembers(member -> {
+				if (member.getRoles().contains(createdRole)) {
+					return true;
+				}
+				return false;
+			}).get();
+			// Remove not qualified members
+			for (int i = 0; i < members.size(); i++) {
+				if (!assignableMembers.contains(members.get(i).getId())) {
+					event.getGuild().removeRoleFromMember(members.get(i), createdRole).queue();
+					System.out.println("Removed member " + members.get(i) + " from regular role");
+				}
+			}
+		} catch (Exception e) {
+			SentryManager.getInstance().handleError(e);
+		} finally {
+			if (SentryManager.getInstance().isActivated()) {
+				transaction.finish();
+			}
+		}
 	}
 }
