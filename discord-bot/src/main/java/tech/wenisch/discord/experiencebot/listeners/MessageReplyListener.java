7282673aedcaed02c9f55a3e8e10029256286a9b
Jean-Fabian Wenisch <fabian@wenisch.tech>
2022-10-22 15:48:28 +0200

    fix: Added info message for onMessageReceived Event

--- discord-bot/src/main/java/tech/wenisch/discord/experiencebot/listeners/MessageReplyListener.java
+++ discord-bot/src/main/java/tech/wenisch/discord/experiencebot/listeners/MessageReplyListener.java
@@ -8,6 +8,7 @@ import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import tech.wenisch.discord.experiencebot.Bot;
 import tech.wenisch.discord.experiencebot.RoleManager;
 import tech.wenisch.discord.experiencebot.SentryManager;
 import tech.wenisch.discord.experiencebot.persistence.DatabaseManager;
@@ -19,26 +20,26 @@ public class MessageReplyListener extends ListenerAdapter {
 	public void onMessageReceived(MessageReceivedEvent event) {
 		Message msg = event.getMessage();
 		MessageChannel channel = event.getChannel();
-		if (msg.getContentRaw().startsWith(commandPrefix)) {
-			String command = msg.getContentRaw().substring(commandPrefix.length()+1).toLowerCase();
-			ITransaction transaction = null;
-
-			if (SentryManager.getInstance().isActivated()) {
-				transaction = Sentry.startTransaction("MessageReceivedEvent", msg.getContentRaw());
-			}
-			try {
-				if (!event.getAuthor().isBot()) {
+		if (!event.getAuthor().isBot()) {
+			if (msg.getContentRaw().startsWith(commandPrefix)) {
+				String command = msg.getContentRaw().substring(commandPrefix.length() + 1).toLowerCase();
+				ITransaction transaction = null;
+				Bot.logger.info("[onMessageReceived] " + event.getMember().getEffectiveName() + " send " + command
+						+ " command at " + event.getGuild().getName());
+				if (SentryManager.getInstance().isActivated()) {
+					transaction = Sentry.startTransaction("MessageReceivedEvent", command);
+				}
+				try {
 
 					if (command.equals("help")) {
 						channel.sendMessage(generateHelpMessage()).queue();
 					} else if (command.equals("level")) {
 						channel.sendMessage(generateLevelResponse(event.getMember().getEffectiveName(),
-								event.getMember().getId(),event.getGuild().getId())).queue();
-					}
-					else if (command.equals("regulars")) {
+								event.getMember().getId(), event.getGuild().getId())).queue();
+					} else if (command.equals("regulars")) {
 						channel.sendMessage(generateRegularsResponse(event.getGuild().getId())).queue();
 					}
-					
+
 //					else if (command.equals("top")) {
 //						channel.sendMessage(generateTopUsersResponse(event.getGuild().getId())).queue();
 //					}
@@ -49,12 +50,13 @@ public class MessageReplyListener extends ListenerAdapter {
 							response.editMessageFormat("pong: %d ms", System.currentTimeMillis() - time).queue();
 						});
 					}
-				}
-			} catch (Exception e) {
-				SentryManager.getInstance().handleError(e);
-			} finally {
-				if (SentryManager.getInstance().isActivated()) {
-					transaction.finish();
+
+				} catch (Exception e) {
+					SentryManager.getInstance().handleError(e);
+				} finally {
+					if (SentryManager.getInstance().isActivated()) {
+						transaction.finish();
+					}
 				}
 			}
 		}
@@ -82,30 +84,30 @@ public class MessageReplyListener extends ListenerAdapter {
 		String totalExp = DatabaseManager.getInstance().getTotalExp(userID, guildID);
 		StringBuilder sb = new StringBuilder();
 		sb.append("Yo " + username + ", ");
-		sb.append(
-				"You are now on lvl " + RoleManager.getLevel(userID, guildID) + " with " + totalExp + " EXP in total. ");
+		sb.append("You are now on lvl " + RoleManager.getLevel(userID, guildID) + " with " + totalExp
+				+ " EXP in total. ");
 		return sb.toString();
 	}
+
 	public static String generateTopUsersResponse(String guildID) {
 		List<String> users = DatabaseManager.getInstance().getTopUsers(guildID);
-		if(users.size()<1)
+		if (users.size() < 1)
 			return "There are currently no Users with EXP on this server";
 		StringBuilder sb = new StringBuilder();
-		for(String username: users)
-		{
-			sb.append(username+"\n");
+		for (String username : users) {
+			sb.append(username + "\n");
 		}
 
 		return sb.toString();
 	}
+
 	public static String generateRegularsResponse(String guildID) {
 		List<String> regulars = DatabaseManager.getInstance().getRegulars(guildID);
-		if(regulars.size()<1)
+		if (regulars.size() < 1)
 			return "There are currently no regulars on this server";
 		StringBuilder sb = new StringBuilder();
-		for(String username: regulars)
-		{
-			sb.append(username+"\n");
+		for (String username : regulars) {
+			sb.append(username + "\n");
 		}
 
 		return sb.toString();
